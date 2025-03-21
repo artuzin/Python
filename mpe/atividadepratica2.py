d88f3873f7e75af3910c602257bcdb44768891ee
artur boudens <85762182+artuzin@users.noreply.github.com>
2025-03-21 09:33:25 -0300

    AP2

--- mpe/atividadepratica2.py
+++ mpe/atividadepratica2.py
@@ -1,2 +1,24 @@
-print("Xereca")
+import pandas as pd
+import matplotlib as plt
+import seaborn as sb
+
+dados = pd.read_csv('C:\\Usuários\\artur\\OneDrive\\Documentos\\GitHub\\Python\\mpe\\TabelaSalarial.csv')
+
+dados = dados.drop(columns=["N"])
+
+colunas_numeros = dados.select_dtypes(include=['number'])
+
+print(dados.head())
+
+mean_values = colunas_numeros.mean()
+median_values = colunas_numeros.median()
+std_values = colunas_numeros.std()
+var_values = colunas_numeros.var()
+range_values = colunas_numeros.max() - colunas_numeros.min()
+
+print("Médias: \n", mean_values)
+print("Medianas: \n", median_values)
+print("Desvio Padrão: \n", std_values)
+print("Variâncias: \n", var_values)
+print("Amplitude: \n", range_values)
 
